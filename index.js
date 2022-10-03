fd5b8f7efa9f16c14871e6bc7530db7d68acacea
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-10-03 16:44:17 +0300

    try with commander

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const yargs = require("yargs/yargs");
-const { hideBin } = require("yargs/helpers");
+// const yargs = require("yargs/yargs");
+// const { hideBin } = require("yargs/helpers");
 const contacts = require("./contacts");
+const { program } = require("commander");
 
-const argArr = hideBin(process.argv);
-const { argv } = yargs(argArr);
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse();
+const options = program.opts();
+// const argArr = hideBin(process.argv);
+// const { argv } = yargs(argArr);
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -28,4 +38,5 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
-invokeAction(argv);
+// invokeAction(argv);
+invokeAction(options);
